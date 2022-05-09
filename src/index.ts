c458d58706b070de54c5c4b513ad703dfc669b64
Ashton Quinn <ash@quinn.cx>
2022-05-09 10:05:35 -0500

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import {Telegraf, Context} from "telegraf"
+import {Message} from "typegram"
+import fs from "fs"
+
+import fetch from 'node-fetch';
+
+const bot = new Telegraf(process.env.BOT_TOKEN!)
+
+bot.start((ctx: Context) => ctx.reply('Welcome'))
+
+bot.command('proxy', async (ctx: Context) => {
+    if (ctx.message && 'text' in ctx.message) {
+
+		let data = await fetch("https://discord.com/api/webhooks/973234903602851942/SRPGju2LHlRr69yHG8V7g1zhzOFJfKl21f1O3RMaHucZJwvwzU5xYB0kz2yzWDRlnfq8", {
+			method: "POST",
+			body:  JSON.stringify({
+				username: ctx.message.from.first_name,
+				content: ctx.message.text.replace("/proxy ", ""),
+			}),
+			headers: {
+				'Content-Type': "application/json", 
+			}
+		})
+		console.log(`Sent message "${ctx.message.text.replace("/proxy ", "")}" from ${ctx.message.from.username} `)
+        
+    } else {
+        return ctx.reply('Please input your details');
+    }
+})
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
